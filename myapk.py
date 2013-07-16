2439057aa56ccf6a7f3ffdffb9a42c82f9cb73da
Cesar Vargas <punisher@new-host-6.home>
2013-07-16 17:03:10 -0400

    deleted spaces and other no needed data

--- myapk.py
+++ myapk.py
@@ -3,14 +3,15 @@ from androguard.core.bytecodes.apk import APK
 
 class MyAPK(APK):
 	def get_sharedUserId(self):
-#		return self.xml["AndroidManifest.xml"].documentElement.getAttribute("android:sharedUserId")
 		return self.get_element('manifest','android:sharedUserId')
+
 	def is_sharedUserIdSet(self):
 		result = False
 		element = self.get_element('manifest','android:sharedUserId')		
 		if element != None:
 			result = True
 		return result
+
 	def get_sharedUserIdInfo(self):
 		userinfo = ( None, None)
 		sharedUserId = self.get_element('manifest','android:sharedUserId')	
