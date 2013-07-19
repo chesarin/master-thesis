37b11594279640cd31a56ec217112d465b5cfee2
Cesar Vargas <punisher@nacolabs.home>
2013-07-19 13:44:30 -0400

    More good coding practices

--- myapk.py
+++ myapk.py
@@ -3,11 +3,13 @@ from androguard.core.bytecodes.apk import APK
 
 class MyAPK(APK):
 	def get_sharedUserId(self):
-		return self.get_element('manifest','android:sharedUserId')
+		return self.get_element('manifest',
+								'android:sharedUserId')
 
 	def is_sharedUserIdSet(self):
 		result = False
-		element = self.get_element('manifest','android:sharedUserId')
+		element = self.get_element('manifest',
+								   'android:sharedUserId')
 		if element != None:
 			result = True
 		return result
