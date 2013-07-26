608ed0006e2311e5f612067231b449a2debe87a8
Cesar Vargas <vargasces@gmail.com>
2013-07-26 15:56:55 -0400

    logging facilities.

--- core/apkdirectoryfactory.py
+++ core/apkdirectoryfactory.py
@@ -15,16 +15,19 @@ class APKDirectoryFactory(IMalwareCorpusFactory):
 	def create(self,directory):
 		listing = os.listdir(directory)
 		for file in listing:
-			temp = directory+'/'+file
+			temp = directory+"/"+file
 			logging.warning("File name "+temp)
 			self.process_apk_file(temp)
 
 	def process_apk_file(self,file):
+		logging.warning("inside process apk files")
 		try:
+			logging.warning("inside the try block")
 			apk = ApkMalware(file)
 			self.ramcorpus.add(apk)
 			self.validapk += 1
 		except:
+			logging.warning("Exception why")
 			self.nonvalidapk += 1
 		
 		
