62ace6f2aa6918a7c989f642e6b6d0f7cdb3dca4
Cesar Vargas <vargasces@gmail.com>
2013-08-12 16:40:43 -0400

    Added zipmetric code to return a float when comparing two malware samples.

--- core/zipmetric.py
+++ core/zipmetric.py
@@ -1,6 +1,49 @@
 from interfaces.idistancemetric import IDistanceMetric
+import zipfile
+import os
+
 
 class ZipMetric(IDistanceMetric):
 
-	def distance(self,f1,f2):
-		return
+	def distance(self,m1,m2):
+		filename = 'combine-zip'
+		with zipfile.ZipFile(filename,'w',zipfile.ZIP_DEFLATED) as czip:
+			czip.write(m1.get_path())
+			czip.write(m2.get_path())
+		czip.close()
+		finfo = os.stat(filename)
+		distance = float(finfo.st_size) / ( m1.get_size() +
+											m2.get_size())
+		print str(finfo.st_size)
+		print str(m1.get_size())
+		print str(m2.get_size())
+		os.remove(filename)
+		return (distance*100)
+
+	def distance2(self,m1,m2):
+		filename1 = 'zip1'
+		with zipfile.ZipFile(filename1,'w',zipfile.ZIP_DEFLATED) as zip1:
+			zip1.write(m1.get_path())
+		zip1.close()
+		filename2 = 'zip2'
+		with zipfile.ZipFile(filename2,'w',zipfile.ZIP_DEFLATED) as zip2:
+			zip2.write(m1.get_path())
+		zip2.close()
+		cfilename = 'combine-zip'
+		with zipfile.ZipFile(cfilename,'w',zipfile.ZIP_DEFLATED) as czip:
+			czip.write(m1.get_path())
+			czip.write(m2.get_path())
+		czip.close()
+		f1info = os.stat(filename1)
+		f2info = os.stat(filename2)
+		cfinfo = os.stat(cfilename)
+		distance = float(cfinfo.st_size) / ( f1info.st_size +
+											 f2info.st_size)
+		print str(cfinfo.st_size)
+		print str(f1info.st_size)
+		print str(f2info.st_size)
+		os.remove(filename1)
+		os.remove(filename2)
+		os.remove(cfilename)
+		return (distance*100)
+
