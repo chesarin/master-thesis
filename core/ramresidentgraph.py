5d2fabda785d1ddf5319713735f3d3cb81356af3
Cesar Vargas <vargasces@gmail.com>
2013-08-13 16:41:50 -0400

    Basic code for RAMResidentGraph object!!

---
+++ core/ramresidentgraph.py
@@ -0,0 +1,38 @@
+import sys
+from interfaces.iphylogeny import IPhylogeny
+from ramresidentmc import RamResidentMC
+
+class RAMResidentGraph(IPhylogeny):
+	
+	def __init__(self):
+		self.corpus = RamResidentMC()
+		self.malware_pairs = []
+
+	def set_corpus(self,incorpus):
+		if self.corpus.get_size() == 0:
+			self.corpus = incorpus
+		else:
+			sys.exit("corpus already set")
+	
+	def add_edge(self,malware1,malware2,distance):
+		if (self.corpus.is_present(malware1) &
+			self.corpus.is_present(malware2)):
+			edge = (malware1,malware2,distance)
+			self.malware_pairs.append(edge)
+		else:
+			sys.exit("malware1 or malware2 not in corpus")
+
+	def write_graphiz_file(self,path):
+		"""Nothing for now"""
+		return
+
+	def get_corpus(self):
+		return self.corpus
+
+	def is_edge(self,malwareu,malwarev):
+		"""Nothing for now"""
+		return
+
+
+
+
