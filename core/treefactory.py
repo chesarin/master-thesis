dca43fe2585b4f41c17002e148c551793bf23929
Cesar Vargas <vargasces@gmail.com>
2013-08-13 16:40:57 -0400

    TreeFactory contructs the tree based on a basic algorithm.

---
+++ core/treefactory.py
@@ -0,0 +1,20 @@
+from interfaces.iphylogenyfactory import IPhylogenyFactory
+from ramresidentgraph import RAMResidentGraph
+class TreeFactory(IPhylogenyFactory):
+
+	def create(self,malwarecorpus,fingerprintfactory,distancemetric):
+		self.RRG = RAMResidentGraph()
+		self.RRG.set_corpus(malwarecorpus)
+
+		for i in range(1,malwarecorpus.get_size()):
+			m = malwarecorpus.getNthCronological(i)
+			mins = float('infinity')
+			for j in range(i):
+				m2 = malwarecorpus.getNthCronological(j)
+				s = distancemetric.distance(fingerprintfactory.create(m),
+											fingerprintfactory.create(m2))
+				if s < mins:
+					mins = s
+					x = m2
+			self.RRG.add_edge(x,m,s)
+		return self.RRG
