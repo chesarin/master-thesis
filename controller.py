1f0ee91f41adb54782b362c848991cdb2ebfe16e
Cesar Vargas <vargasces@gmail.com>
2013-08-14 15:25:28 -0400

    Started using the ArgumentParser class to get arguments from the command line. I still need to get more familiar with it. The following is the link to the ArgumentParser object. http://docs.python.org/dev/library/argparse.html http://docs.python.org/dev/library/argparse.html#action http://docs.python.org/dev/library/argparse.html#argparse.ArgumentParser.add_argument

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #from core.apkdirectoryfactory import APKDirectoryFactory
 import logging
+import argparse
 from core.testmalwaredirectoryfactory import TestMalwareDirectoryFactory
 from core.losslessfingerprintfactory import LosslessFingerPrintFactory
 from core.zipmetric import ZipMetric
@@ -8,16 +9,36 @@ from core.treefactory import TreeFactory
 
 
 def main():
+	log = logging.getLogger(__name__) 
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-q","--quiet",
+						help="Display only error messages",
+						action="store_true",required=False)
+	parser.add_argument("-d","--debug",
+						help="Display debug messages",
+						action="store_true",required=False)
+	parser.add_argument("-dir","--directory",
+						help="Directory to read malware from",
+						default="/Users/punisher/Documents/master-thesis/data",
+						required=False)
+	args = parser.parse_args()
+	dic_args = vars(args)
+
+#	if args.quiet:
+#		log.setLevel(logging.WARN)
+#	elif args.debug:
+#		log.setLevel(logging.DEBUG)
+
 	logging.basicConfig(filename='application.log',
 						filemode='w',
-						level=logging.INFO,
+						level=logging.DEBUG,
 						format='%(asctime)s %(name)s %(message)s', 
 						datefmt='%m/%d/%Y %I:%M:%S %p')
-	log = logging.getLogger(__name__) 
+
+
 	log.info('Controller Start')
-	directory1 = '/Users/punisher/Documents/master-thesis/data'
 	dfactory = TestMalwareDirectoryFactory()
-	dfactory.create(directory1)
+	dfactory.create(dic_args['directory'])
 	mc = dfactory.get_corpus()
 	fpf = LosslessFingerPrintFactory()
 	dis = ZipMetric()
