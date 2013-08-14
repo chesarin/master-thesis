32643368f93ff9b283ead4df6d5c82019ca5745a
Cesar Vargas <vargasces@gmail.com>
2013-08-14 13:22:12 -0400

    logging functions to add logs to a file and other formatting parameters.

--- controller.py
+++ controller.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 #from core.apkdirectoryfactory import APKDirectoryFactory
+import logging
 from core.testmalwaredirectoryfactory import TestMalwareDirectoryFactory
 from core.losslessfingerprintfactory import LosslessFingerPrintFactory
 from core.zipmetric import ZipMetric
 from core.treefactory import TreeFactory
 
+
 def main():
-	print 'Contorller Start'
+	logging.basicConfig(filename='application.log',
+						filemode='w',
+						level=logging.INFO,
+						format='%(asctime)s %(name)s %(message)s', 
+						datefmt='%m/%d/%Y %I:%M:%S %p')
+	log = logging.getLogger(__name__) 
+	log.info('Controller Start')
 	directory1 = '/Users/punisher/Documents/master-thesis/data'
 	dfactory = TestMalwareDirectoryFactory()
 	dfactory.create(directory1)
@@ -15,7 +23,7 @@ def main():
 	dis = ZipMetric()
 	treefactory = TreeFactory()
 	result = treefactory.create(mc,fpf,dis)
-	print 'Controller Ends'
+	log.info('Controller Ends')
 
 if __name__ == "__main__":
 	main()
