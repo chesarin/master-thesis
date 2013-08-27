e911049060cade25b1248b38332ab52ba9629511
Cesar Vargas <vargasces@gmail.com>
2013-08-27 13:34:02 -0400

    ramresidentgraph has enough to create a graphiz along the distance value presented on the edge.

--- core/ramresidentgraph.py
+++ core/ramresidentgraph.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import pydot
 from interfaces.iphylogeny import IPhylogeny
 from ramresidentmc import RamResidentMC
 
@@ -10,6 +11,7 @@ class RAMResidentGraph(IPhylogeny):
 	def __init__(self):
 		self.corpus = RamResidentMC()
 		self.malware_pairs = []
+		self.corpus_hash = {}
 
 	def set_corpus(self,incorpus):
 		if self.corpus.get_size() == 0:
@@ -25,19 +27,38 @@ class RAMResidentGraph(IPhylogeny):
 		else:
 			sys.exit("malware1 or malware2 not in corpus")
 
-	def write_graphiz_file(self,path):
-		"""Nothing for now"""
-		return
-
+	def write_graphiz_file(self,filename):
+		if len(self.corpus_hash) == 0:
+			self.create_corpus_hash()
+		graph = pydot.Dot(graph_type='digraph')
+		for i in self.malware_pairs:
+			node1 = self.get_location(i[0])
+			node2 = self.get_location(i[1])
+			edge = pydot.Edge(node1,node2,label=str(i[2]))
+			graph.add_edge(edge)
+		graph.write_png(filename)
+		
+	def create_corpus_hash(self):
+		for location in range(self.corpus.get_size()):
+			self.corpus_hash[self.corpus.getNthCronological(location)] = location
+	
 	def get_corpus(self):
 		return self.corpus
+
+	def get_location(self,malware):
+		return self.corpus_hash[malware]
 
 	def is_edge(self,malwareu,malwarev):
 		"""Nothing for now"""
 		return
 
 	def print_edges(self):
+		self.create_corpus_hash()
 		for i in self.malware_pairs:
-			log.info( '%s %s %s', i[0], i[1], i[2])
+			log.info( '%s %s %s', self.get_location(i[0]),
+					self.get_location(i[1]), i[2])
+
+
+
 
 
