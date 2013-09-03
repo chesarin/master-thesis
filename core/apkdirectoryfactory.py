3526707eb5cb18f36462552481e112f6c9653770
Cesar Vargas <vargasces@gmail.com>
2013-09-03 17:01:48 -0400

    4 space tabs

--- core/apkdirectoryfactory.py
+++ core/apkdirectoryfactory.py
@@ -7,29 +7,29 @@ from apkmalware import ApkMalware
 
 class APKDirectoryFactory(IMalwareCorpusFactory):
 
-	def __init__(self):
-		self.ramcorpus = RamResidentMC()
-		self.validapk = 0
-		self.nonvalidapk = 0
+    def __init__(self):
+        self.ramcorpus = RamResidentMC()
+        self.validapk = 0
+        self.nonvalidapk = 0
 
-	def create(self,directory):
-		listing = os.listdir(directory)
-		for file in listing:
-			temp = directory+"/"+file
-			logging.warning("File name "+temp)
-			self.process_apk_file(temp)
+    def create(self,directory):
+        listing = os.listdir(directory)
+        for file in listing:
+            temp = directory+"/"+file
+            logging.warning("File name "+temp)
+            self.process_apk_file(temp)
 
-	def process_apk_file(self,file):
-		logging.warning("inside process apk files")
-		try:
-			logging.warning("Creating APK object")
-			apk = ApkMalware(file)
-			logging.warning("Adding APK object to ramcorpus")
-			self.ramcorpus.add(apk)
-			logging.warning("incrementing counter for validapk")
-			self.validapk += 1
-		except:
-			logging.warning("Exception why")
-			self.nonvalidapk += 1
-		
-		
+    def process_apk_file(self,file):
+        logging.warning("inside process apk files")
+        try:
+            logging.warning("Creating APK object")
+            apk = ApkMalware(file)
+            logging.warning("Adding APK object to ramcorpus")
+            self.ramcorpus.add(apk)
+            logging.warning("incrementing counter for validapk")
+            self.validapk += 1
+        except:
+            logging.warning("Exception why")
+            self.nonvalidapk += 1
+        
+        
