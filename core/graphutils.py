9a78914ca28bdc47ed50b5ddc14efe10a877343e
Cesar Vargas <vargasces@gmail.com>
2013-09-13 07:24:36 -0400

    graphutils is module that includes the class GraphJson to create a json file or json object. It takes as input a normal graph from Graph class and it created a dictionary representing json output. The json output can be written to a file by passing a filename to the create_json_file function. Please note that the create_dict function depends on the following function from a Graph object: get_graph() - to get the actual graph
    Once the actual graph has been obtained the following functions are needed:
    
    successors(node)-get the list of successors/children for a specific node
    get_root() - get the actual root of the graph.
    
    Note:
    Future implementations should take care of multiple roots. A forest or network of trees.

---
+++ core/graphutils.py
@@ -0,0 +1,26 @@
+import logging
+import json
+
+log = logging.getLogger(__name__)
+
+class GraphJson(object):
+    def __init__(self,Graph):
+        self.Graph = Graph.get_graph()
+    def create_dict(self,node):
+        if not len(self.Graph.successors(node)):
+            return {
+                'id':str(node),
+                'name':str(node)[:7]
+            }
+        return {
+            'id':str(node),
+            'name':str(node)[:7],
+            'chidren':map(self.create_dict,self.Graph.successors(node))
+        }
+    def create_json_file(self,filename):
+        root = self.Graph.get_root()
+        log.info('print root %s',str(root))
+        data = self.create_dict(root)
+        log.info('writing the following to filename: %s',data)
+        with open(filename,'wb') as fp:
+            json.dump(data,fp,sort_keys=True,indent=1,separators=(',',':'))
