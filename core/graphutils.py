2173bcaa531bb4e8e39df63d35f26a469c64fee3
Cesar Vargas <vargasces@gmail.com>
2013-09-13 16:57:09 -0400

    logging feature...trying to add the date to the node object so we can use it on the graphing software.

--- core/graphutils.py
+++ core/graphutils.py
@@ -5,19 +5,36 @@ log = logging.getLogger(__name__)
 
 class GraphJson(object):
     def __init__(self,Graph):
+        log.info('initialing GraphJson')
         self.Graph = Graph.get_graph()
+        log.info("let's iterate over the nodes in this graph")
+        for node in self.Graph.iternodes():
+            log.info('node %s and date %s',
+                     str(node),
+                     str(node.attr['comment']))
     def create_dict(self,node):
+        log.info('node id:%s date:%s',
+                 str(node),
+                 str(node.attr['comment']))
         if not len(self.Graph.successors(node)):
             return {
                 'id':str(node),
-                'name':str(node)[:7]
+                'name':''
+                # 'data':{
+                #     'date':str(node.attr['comment'])
+                # }
             }
         return {
             'id':str(node),
-            'name':str(node)[:7],
-            'children':map(self.create_dict,self.Graph.successors(node))
+            'name':'',
+            # 'data':{
+            #     'date':str(node.attr['comment'])
+            # },
+            'children':map(self.create_dict,
+                           self.Graph.successors_iter(node))
         }
     def create_json_file(self,filename):
+        log.info('creating json file at %s',filename)
         root = self.Graph.get_root()
         log.info('print root %s',str(root))
         data = self.create_dict(root)
