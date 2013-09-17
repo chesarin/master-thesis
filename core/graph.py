8e934a0bfe65ec27e15721912c5dd8f307a9c271
Cesar Vargas <vargasces@gmail.com>
2013-09-16 21:25:19 -0400

    Graph should return a list of nodes with no children so we can start creating the file to plot into infovis library.

--- core/graph.py
+++ core/graph.py
@@ -5,10 +5,11 @@ log = logging.getLogger(__name__)
 
 class Graph(pgv.AGraph):
     def get_root(self):
+        roots = []
         for n,d in self.in_degree_iter():
             if d is 0:
-                root = n
-        return root    
+                roots.append(n)
+        return roots    
     def get_number_of_children(self,node):
         return len(self.successors(node))
 
