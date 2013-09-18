7f9f86aa2e9dfb70c9265fdadb37320d3c98af99
Cesar Vargas <vargasces@gmail.com>
2013-09-18 16:51:48 -0400

    ratcliffmetric uses the difflib library in python. However, the following resources are important to understand how this metric works.
    http://xlinux.nist.gov/dads/HTML/ratcliffObershelp.html
    http://docs.python.org/2/library/difflib.html#difflib.SequenceMatcher
    http://www.drdobbs.com/database/pattern-matching-the-gestalt-approach/184407970?pgno=5

---
+++ core/ratcliffmetric.py
@@ -0,0 +1,15 @@
+from interfaces.idistancemetric import IDistanceMetric
+import difflib
+import logging
+
+log = logging.getLogger(__name__)
+class RatcliffMetric(IDistanceMetric):
+    """
+    http://xlinux.nist.gov/dads/HTML/ratcliffObershelp.html
+    http://docs.python.org/2/library/difflib.html#difflib.SequenceMatcher
+    http://www.drdobbs.com/database/pattern-matching-the-gestalt-approach/184407970?pgno=5
+    """
+    def distance(self,a,b):
+        seq = difflib.SequenceMatcher(a=a.get_xml().lower(),
+                                      b=b.get_xml().lower())
+        return 1-seq.ratio()
