69a7735b12e89453a67c8d03fd40d4c60f743d21
Cesar Vargas <punisher@lanegra-linux>
2014-01-24 15:50:06 -0500

    Moving directory libraries to /lib/apk

--- processapk.py
+++ processapk.py
@@ -11,6 +11,7 @@ from time import mktime
 from controller import execute
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from lib.apk.processor import APKDirectoryFactory
 # from timeit import timeit
 import timeit
 log = logging.getLogger(__name__)
@@ -98,22 +99,6 @@ class Sampler(object):
             if (os.path.isfile(sample.get_filename())):
                 shutil.copy(sample.get_filename(),full_dest_path)
                 
-class APKFile(object):
-    def __init__(self,filename):
-        self.filename = filename
-        self.zip = zipfile.ZipFile(filename,'r')
-        self.date = self.zip.getinfo('AndroidManifest.xml').date_time
-        self.zip.close()
-        
-    def get_filename(self):
-        return self.filename
-
-    def get_date(self):
-        return datetime(*self.date[0:6])
-        
-    def __str__(self):
-        return '%s %s' %(str(self.filename.rsplit('/')[-1]), str(self.date))
-
 class APKCorpus(object):
     def __init__(self):
         self.corpus = []
@@ -129,24 +114,6 @@ class APKCorpus(object):
 
     def get_element(self,number):
         return self.corpus[number]
-        
-class APKDb(object):
-    def __init__(self):
-        log.info('Creating APKdb')
-        self.db = []
-    def add(self,apkfile):
-        self.db.append(apkfile)
-    def sort_db(self):
-        self.db.sort(key=lambda apkfile:apkfile.get_date())
-    def __iter__(self):
-        return iter(self.db)
-    def get_size(self):
-        self.sort_db()
-        return len(self.db)
-    def get_based_date(self):
-        return self.db[0].get_date()
-    def get_last_item(self):
-        return self.db[-1]
         
 class APKDbDate(object):
     def __init__(self):
@@ -176,40 +143,6 @@ class APKDbDate(object):
         
     def get_samples_on(self,year):
         return self.db[year]
-        
-class APKDirectoryFactory(object):
-
-    # def __init__(self):
-    #     self.apkcorpus
-        
-    def set_corpus(self,corpus):
-        self.apkcorpus = corpus
-        
-    def create(self,directory):
-        listing = os.listdir(directory)
-        for file in listing:
-            temp = directory+"/"+file
-            self.process_apk_file(temp)
-            
-    def process_apk_file(self,file):
-        try:
-            log.info('starting apk object creation %s',file)
-            apk = APKFile(file)
-            log.info('Created apk object and adding to corpus')
-            log.info('date %s',str(apk.get_date()))
-            self.apkcorpus.add(apk)
-        except Exception, e :
-            log.info('exception %s',file)
-            log.exception(e) 
-            
-    def get_size(self):
-        return self.apkcorpus.get_size()
-
-    def get_corpus(self):
-        return self.apkcorpus
-            
-    def get_stats(self):
-        self.apkcorpus.get_stats()
         
 class APKDirectoryExtractor(object):
     
@@ -256,12 +189,12 @@ class APKDirectoryExtractor(object):
             if (os.path.isfile(element.get_filename())):
                 shutil.copy(element.get_filename(),full_dest_file_path)
         
-def create_factory(inputdirectory,corpus):
-    input = APKDirectoryFactory()
-    input.set_corpus(corpus)
-    input.create(inputdirectory)
-    # input.get_stats()
-    return input
+# def create_factory(inputdirectory,corpus):
+#     input = APKDirectoryFactory()
+#     input.set_corpus(corpus)
+#     input.create(inputdirectory)
+#     # input.get_stats()
+#     return input
     
 def init_logging(args):
     timestr = time.strftime("%Y%m%d-%H%M")
@@ -316,8 +249,9 @@ def main():
     init_logging(args)
     """Actual Works Start Here"""
     log.info('Starts')
-    corpus = APKDb()
-    factory = create_factory(args.inputdirectory,corpus)
+    # corpus = APKDb()
+    # factory = create_factory(args.inputdirectory,corpus)
+    factory = APKDirectoryFactory(args.inputdirectory)
     log.info('%s %s','size of factory',str(factory.get_size()))
     corpus = factory.get_corpus()
     sdate = corpus.get_based_date()
