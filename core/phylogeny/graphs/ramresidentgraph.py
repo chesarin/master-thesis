d6939aa4758f2def34ef3b3b59f3b90afb09aef7
Cesar Vargas <punisher@lanegra-linux>
2014-03-12 16:26:56 -0400

    fixed spaces in parameters.

--- core/phylogeny/graphs/ramresidentgraph.py
+++ core/phylogeny/graphs/ramresidentgraph.py
@@ -15,7 +15,7 @@ class RAMResidentGraph(IPhylogeny):
         self.corpus_hash = {}
         self.graph = Graph()
 
-    def set_corpus(self,incorpus):
+    def set_corpus(self, incorpus):
         """Set the corpus from the incorpus object. Once that is
         done then sort the corpus from earliest to latest sample than
         create a hash for lookup locations. 
@@ -41,16 +41,21 @@ class RAMResidentGraph(IPhylogeny):
             sys.exit("malware not in corpus")
 
     def add_edge(self,malware1,malware2,distance):
-        log.info('parameters:%s %s %s',malware1,malware2,str(distance))
+        log.info('parameters:%s %s %s',
+                 malware1,
+                 malware2,
+                 str(distance))
         if (self.corpus.is_present(malware1) &
             self.corpus.is_present(malware2)):
             #Avoid adding cycles
             if not malware1 == malware2:
                 log.info('adding edge as follows')
-                log.info('malware 1 %s with date %s',malware1,str(malware1.get_date()))
-                log.info('malware 2 %s with date %s',malware2,str(malware2.get_date()))
-                # self.graph.add_node(malware1,label=str(malware1),comment=str(malware1.get_date()))
-                # self.graph.add_node(malware2,label=str(malware2),comment=str(malware2.get_date()))
+                log.info('malware 1 %s with date %s',
+                         malware1,
+                         str(malware1.get_date()))
+                log.info('malware 2 %s with date %s',
+                         malware2,
+                         str(malware2.get_date()))
                 self.graph.add_node(malware1,comment=str(malware1.get_date()))
                 self.graph.add_node(malware2,comment=str(malware2.get_date()))
                 self.graph.add_edge(malware1,malware2,label=str(distance))
@@ -73,12 +78,13 @@ class RAMResidentGraph(IPhylogeny):
             counter += 1
         return self.graph
         
-    def write_graphviz(self,outputdir='output',name='phylogeny'):
-        log.info('trying to write graphviz file for graph with nodes %s',str(self.graph.number_of_nodes()))
+    def write_graphviz(self, outputdir='output', name='phylogeny'):
+        log.info('trying to write graphviz file for graph with nodes %s',
+                 str(self.graph.number_of_nodes()))
         timestr = time.strftime("%Y%m%d-%H%M%S")
         directory = outputdir + '/graphviz/'
         if not os.path.exists(directory):
             log.info('path does not exist so create directory for predictions')
             os.makedirs(directory)
-        filename = directory + 'graphviz-'+name+'-'+ timestr+'.dot'
+        filename = directory + 'graphviz-' + name + '-' + timestr + '.dot'
         self.graph.write(filename)
